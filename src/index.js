061a52abde4e36e2836ea2133220d794bb12f099
Bruno Krebs <bruno.krebs@auth0.com>
2018-04-12 23:43:33 -0300

    starting to create nodez-server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const Docker = require('dockerode');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+app.get('/', async (req, res) => {
+  docker.listContainers(function (err, containers) {
+    if (err) return console.log(err);
+
+    containers.forEach(function (containerInfo) {
+      console.log(containerInfo);
+      res.send('Hello World!');
+    });
+  });
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
